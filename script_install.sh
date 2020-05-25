98fbf2d51fdb6fd7165dd289a69802709a3c261e
root <root@ip-172-31-57-209.ec2.internal>
2020-05-25 07:55:48 +0000

    spring-boot-hello

--- script_install.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cd /home/ec2-user/application
-sudo ./mvnw install
--- script_start_server.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cd /home/ec2-user/application
-sudo ./mvnw spring-boot:run > /dev/null 2> /dev/null < /dev/null &
